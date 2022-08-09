07a65d55f5dd3238556429c8e922a9cb60d8d6ed
Lars Erik Bratlie <larse.bratlie@gmail.com>
2022-08-09 14:50:03 +0200

    added queue

---
+++ src/DataStructures/Queue.test.ts
@@ -0,0 +1,84 @@
+import { expect, describe, it } from "bun:test";
+import { randomArrayGenerator } from "src/Common/test-tools/ArraysTools";
+import Queue from "./Queue";
+
+describe("Queue: this is tested with 100.000 random numbers", function () {
+  const numOfEl = 100000;
+  const rangeOfNum = 100000;
+  const randomArray = randomArrayGenerator(rangeOfNum, numOfEl, 0, true);
+  const q = new Queue<number>();
+  let i;
+
+  it("Queue Should start of empty", function () {
+    const q = new Queue<number>();
+    expect(q.isEmpty()).toBe(true);
+    expect(q.size()).toBe(0);
+  });
+
+  it("Queue Should iterate correctly", function () {
+    let qu = new Queue<number>();
+    qu.enqueue(3);
+    qu.enqueue(2);
+    qu.enqueue(1);
+    let comArr = [3, 2, 1];
+    for (let el of q) {
+      expect(el).toBe(comArr.shift());
+    }
+  });
+
+  it("Enqueue and Size should return correct amount of elements in q after adding", function () {
+    for (let i = 0; i < numOfEl; i++) {
+      expect(q.enqueue(randomArray[i])).toBe(i + 1);
+      expect(q.size()).toBe(i + 1);
+    }
+  });
+
+  it("Dequeue should return the right elements, dequeue all elements.", function () {
+    for (i = 0; i < numOfEl; i++) {
+      expect(q.dequeue()).toBe(randomArray[i]);
+    }
+  });
+
+  it("Queue should be empty after dequeueing all elements", function () {
+    expect(q.isEmpty()).toBe(true);
+    expect(q.size()).toBe(0);
+  });
+
+  it("Should enque and dequeue correctly", function () {
+    var to = numOfEl / 2;
+    for (i = 0; i < to; i++) {
+      expect(q.enqueue(randomArray[i])).toBe(i + 1);
+    }
+
+    expect(q.size()).toBe(numOfEl / 2);
+
+    for (i = 0; i < to; i++) {
+      expect(q.dequeue()).toBe(randomArray[i]);
+    }
+
+    expect(q.isEmpty()).toBe(true);
+    expect(q.size()).toBe(0);
+
+    for (i = 0; i < numOfEl; i++) {
+      expect(q.enqueue(randomArray[i])).toBe(i + 1);
+    }
+
+    for (i = 0; i < numOfEl; i++) {
+      expect(q.dequeue()).toBe(randomArray[i]);
+    }
+
+    expect(q.isEmpty()).toBe(true);
+    expect(q.size()).toBe(0);
+  });
+
+  it("Peek should always point at the first element in line", function () {
+    for (i = 0; i < randomArray.length; i++) {
+      q.enqueue(randomArray[i]);
+      expect(q.peekFirst()).toBe(randomArray[0]);
+    }
+    for (; i > 0; i--) {
+      var peek = q.peekFirst();
+      expect(q.dequeue()).toBe(peek);
+    }
+  });
+});
---
+++ src/DataStructures/Queue.ts
@@ -0,0 +1,90 @@
+import LinkedList from "./LinkedList";
+/**
+ * Class representing a queue. This representation uses an LinkedList as base structure, it uses Linked Lists unshift and pop for
+ * enqueueing and dequeueing. This class does not have any limitiations on how many elements there can be in the list.
+ * @class
+ */
+export default class Queue<T> {
+  private q: LinkedList<T>;
+  /**
+   * constructor - makes a array as a base structure
+   */
+  constructor() {
+    this.q = new LinkedList<T>();
+    this[Symbol.iterator] = this.q[Symbol.iterator].bind(this.q);
+  }
+
+  /**
+   * enqueue - enqueues at the end of the queue
+   *
+   * @param  {T} item to enqueue
+   * @return {number} The size of the new queue
+   */
+  enqueue(item: T) {
+    return this.q.push(item);
+  }
+
+  /**
+   * dequeue - dequeues the first item in the queue
+   *
+   * @return {Object}  The first object in the queue
+   * @todo throw error when empty
+   */
+  dequeue() {
+    if (this.isEmpty()) {
+      var e = new Error();
+      e.name = "EmptyQueueError";
+      e.message = "Cannot dequeue, queue is empty";
+      throw e;
+    }
+    return this.q.shift();
+  }
+
+  /**
+   * peekFirst - peeks at the first element in line
+   *
+   * @return {Object}  returns the first element in line.
+   */
+  peekFirst() {
+    if (this.isEmpty()) {
+      var e = new Error();
+      e.name = "EmptyQueueError";
+      e.message = "Cannot peek, queue is empty";
+      throw e;
+    }
+    return this.q.peekFirst();
+  }
+
+  /**
+   * peekLast - peeks at the first element in line
+   *
+   * @return {Object}  returns the first element in line.
+   */
+  peekLast() {
+    if (this.isEmpty()) {
+      var e = new Error();
+      e.name = "EmptyQueueError";
+      e.message = "Cannot peek, queue is empty";
+      throw e;
+    }
+    return this.q.peekLast();
+  }
+
+  /**
+   * size - gets the size of the queue.
+   *
+   * @return {number}  size of the queue.
+   */
+  size() {
+    return this.q.size();
+  }
+
+  /**
+   * isEmpty - Checks if the stack is empty
+   *
+   * @return {Boolean} The index of the top element
+   */
+  isEmpty() {
+    return this.q.size() === 0 ? true : false;
+  }
+}
